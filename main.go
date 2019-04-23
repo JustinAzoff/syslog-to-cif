4e807d37962184f7b6563880ef55a9aeda177630
Justin Azoff <justin.azoff@gmail.com>
2019-04-23 16:00:15 -0400

    initial program based on json perf

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+//For pushing up to main using channels?
+type Notice struct {
+	Src string `json:"src"`
+	Dst string `json:"dst"`
+}
+
+func listen(addr string, port int, noticeChan chan Notice) {
+	bind := fmt.Sprintf("%s:%d", addr, port)
+	log.Printf("Listening on %s", bind)
+	l, err := net.Listen("tcp", bind)
+	if err != nil {
+		log.Fatalf("Error listening: %v", err)
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Error accepting: %v", err)
+		}
+		log.Printf("New connection from %s", conn.RemoteAddr())
+		go handleLog(conn, noticeChan)
+	}
+}
+
+func handleLog(conn net.Conn, noticeChan chan Notice) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(file)
+	buf := make([]byte, 4096)
+	for scanner.Scan() {
+		fmt.Println("Got " + scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Error(err)
+	}
+}
+
+func receive(addr string, port int) {
+	noticeBuffer := make([]int, 0, 100) // len()=0, cap()=100
+	noticeChan := make(chan Notice, 100)
+	go listen(addr, port, noticeChan)
+
+	ticker := time.NewTicker(10 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			for _, n := range noticeBuffer {
+				log.Print(s)
+			}
+			noticeBuffer = noticeBuffer[:0]
+		case note := <-noticeBuffer:
+			noticeBuffer = append(noticeBuffer, note)
+		}
+	}
+}
+
+func main() {
+	var port int
+	var addr string
+	var interval time.Duration
+	flag.StringVar(&addr, "addr", "0.0.0.0", "Address to listen on")
+	flag.IntVar(&port, "port", 9000, "Port to listen on")
+	flag.Parse()
+	receive(addr, port)
+}
